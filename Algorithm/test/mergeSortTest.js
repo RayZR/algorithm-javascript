724307eaaee1ae8bbe5afbcd94281de2e5a40c28
Ray Zhang <ray@LamaLama.local>
2016-10-13 15:29:14 +1100

    update

---
+++ Algorithm/test/mergeSortTest.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ray on 13/10/2016.
+ */
+
+var chai   = require('chai');
+var mergeSort = require('../src/mergeSort');
+var assert = chai.assert;
+
+
+    describe('mergeSort', function() {
+        var sortingArray = [];
+        before(function () {
+            for(var i=0; i< 90; i++){
+                sortingArray[i] =Math.floor((Math.random() * 500) + 1);
+            }
+        });
+
+        it('The array should be sorted', function() {
+            assert.equal( sortingArray.sort() , mergeSort(sortingArray) );
+        });
+
+    });
--- Algorithm/trampoline.js
+++
@@ -1,9 +0,0 @@
-L.trampoline = function(fun /*, args */) {
-  var result = fun.apply(fun, _.rest(arguments));
-
-  while (_.isFunction(result)) {
-    result = result();
-  }
-
-  return result;
-};
