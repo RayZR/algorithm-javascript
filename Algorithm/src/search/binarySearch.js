0409915721ce1bc9abfe2d2c5e7b3e7ef5022185
Ray Zhang <ray@LamaLama.local>
2016-12-21 16:12:33 +1100

    update binary search Insert

--- Algorithm/src/search/binarySearch.js
+++ Algorithm/src/search/binarySearch.js
@@ -1,45 +1,38 @@
-var binarySearch = function(inputArray, number){
+var searchInsert = function(inputArray, number){
 
-    var start = 0, end = inputArray.length-1;
+    var start = 0, end = inputArray.length;
 
     while( start <= end ){
 
-        middle = start + Math.floor( (end - start)/2 );
+        if( end <= 0 || start >= parseInt( inputArray.length - 1 ) ){
 
-        if( end === 0){
+            if( number < inputArray[0]){
 
-            return 0;
-        }else if( start === inputArray.length-1){
+                return 0;
+            }else if( number > inputArray[ inputArray.length - 1 ] ) {
 
-            return inputArray.length-1;
+                return inputArray.length;
+            }
         }
 
-        console.log( "start: " + start );
-        console.log( "end: " + end );
-        console.log( "middle: " + middle );
+        middle = start + Math.floor( (end - start)/2 );
 
         if( number > inputArray[ middle ] ){
 
-            console.log( "bigger" );
-            console.log( "index: " + parseInt( middle ) );
-            console.log( "value: " + parseInt( inputArray[ parseInt( middle ) ] ) );
-            console.log( "index: " + parseInt( middle + 1  ) );
-            console.log( "value: " + parseInt( inputArray[ parseInt( middle + 1 ) ] ) );
+            if( ( parseInt(  middle + 1 ) <= end  ) &&  ( number < inputArray[ parseInt( middle + 1 ) ] ) ){
 
-            if( ( parseInt(  middle + 1 ) < end  ) &&  ( number < inputArray[ parseInt( middle + 1 ) ] ) ){
-
-                return middle;
+                return middle + 1;
             }
-                start = parseInt( middle + 1 );
-                console.log("new start: " + start);
+            start = parseInt( middle + 1 );
+
         }else if( number < inputArray[  middle ] ){
 
             if( ( parseInt( middle - 1 ) >= start ) && ( number > inputArray[ parseInt( middle - 1 ) ]) ){
 
-                return parseInt( middle - 1 );
+                return parseInt( middle  );
             }
-                end = parseInt( start + end - middle -1 );
-                console.log("new end: " + end);
+            end = parseInt( start + end - middle -1 );
+
         }else{
 
             return middle;
@@ -51,10 +44,10 @@ var testArray = [1, 7, 14, 17, 18, 19, 19, 19, 20, 20, 21, 24, 28, 29, 31, 31, 3
 
 console.log( "Array Length: " + testArray.length);
 
-console.log( binarySearch(testArray, 23) + " ================================== 10" );
-console.log( binarySearch(testArray, 24) + " ================================== 11" );
-console.log( binarySearch(testArray, 27) + " ================================== 11" );
-console.log( binarySearch(testArray, 36) + " ================================== 18" );
-console.log( binarySearch(testArray, 31) + " ================================== 13 or 14" );
-console.log( binarySearch(testArray, 47) + " ================================== 23" );
-console.log( binarySearch(testArray, 0)  + " ================================== 0" );
+console.log( searchInsert(testArray, 23) + " ================================== 11" );
+console.log( searchInsert(testArray, 24) + " ================================== 12" );
+console.log( searchInsert(testArray, 27) + " ================================== 12" );
+console.log( searchInsert(testArray, 36) + " ================================== 19" );
+console.log( searchInsert(testArray, 31) + " ================================== 14 or 15" );
+console.log( searchInsert(testArray, 47) + " ================================== 24" );
+console.log( searchInsert(testArray, 0)  + " ================================== 0" );
